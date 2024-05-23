a9ff906a7f0b7f726ffb051a95233a655fb07054
cobendaniel <78524895+cobendaniel@users.noreply.github.com>
2024-05-23 15:46:51 +0900

    Update LotteryGUI.py

--- LotteryGUI.py
+++ LotteryGUI.py
@@ -1,4 +1,3 @@
-
 import threading, playsound, random
 
 import datetime as dt 
@@ -9,6 +8,7 @@ from BasicMethods import BasicMethods
 class LotteryGUI(BasicMethods):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.reset_number = 1
 
     def lottery_gui_settings_method(self, sound_effect_file: str, names_list: str):
         self.sound_effect_file  = sound_effect_file
@@ -18,33 +18,42 @@ class LotteryGUI(BasicMethods):
         self.winners_list       = f"./{str(dt.datetime.now().date())}_winners_list.txt"
 
     def __countdown(self, status_window):
-        def update_countdown(count_in_seconds: int):
+        def countdown_logic(status_window, count_in_seconds: int):
+            status_window.delete("1.0", "end")
+            status_window.insert("1.0", f"\n{count_in_seconds}")
+            status_window.tag_add("tag_name", "1.0", "end")
+            count_in_seconds -= 1
+            status_window.after(1000, update_countdown, count_in_seconds, status_window)
+
+        def generate_random_name(status_window):
+            random_name = random.choice(self.company_names_list)
+            self.company_names_list.remove(random_name)
+            print(len(self.company_names_list))
+            status_window.delete("1.0", "end")
+            status_window.insert("1.0", f"\n{random_name[0]} {random_name[1]}")
+            status_window.tag_add("tag_name", "1.0", "end")
+
+            return random_name
+
+        def update_countdown(count_in_seconds: int, status_window):
             if (count_in_seconds > 0):
-                status_window.delete("1.0", "end")
-                status_window.insert("1.0", f"\n{count_in_seconds}")
-                status_window.tag_add("tag_name", "1.0", "end")
-                count_in_seconds -= 1
-                status_window.after(1000, update_countdown, count_in_seconds)
+                countdown_logic(status_window, count_in_seconds)
             else:
-                random_name = random.choice(self.company_names_list)
-                self.company_names_list.remove(random_name)
-                print(len(self.company_names_list))
-                status_window.delete("1.0", "end")
-                status_window.insert("1.0", f"\n{random_name[0]} {random_name[1]}")
-                status_window.tag_add("tag_name", "1.0", "end")
+                random_name = generate_random_name(status_window)
 
                 with open(self.winners_list, "a", encoding = "utf-8") as f:
-                    f.write(f"{str(dt.datetime.now())[0:16]} {random_name[0]} {random_name[1]}\n")
+                    f.write(f"Attempt {self.reset_number}: {str(dt.datetime.now())[0:16]} {random_name[0]} {random_name[1]}\n")
 
-        update_countdown(5)
+        update_countdown(5, status_window)
     
     def __backend_logic(self, status_window):
-        threading.Thread(target = playsound.playsound, args=(self.sound_effect_file,)).start()
+        threading.Thread(target = playsound.playsound, args=(self.sound_effect_file, )).start()
         self.__countdown(status_window)
 
     def __reset_function(self, status_window):
         status_window.delete("1.0", "end")
         self.lottery_gui_settings_method(self.sound_effect_file, self.names_list_file)
+        self.reset_number += 1
 
     def random_name_generator(self):
         label_object  = self.create_label(" ", 0.05, 0.03)
